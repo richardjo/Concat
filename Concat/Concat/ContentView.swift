26060a83b09e3abf34294032a9367c24db8750e3
Richard Jo <jorichard0123@gmail.com>
2020-09-13 21:48:56 -0500

    first commit

---
+++ Concat/Concat/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Concat
+//
+//  Created by Richard Jo on 9/13/20.
+//  Copyright © 2020 Richard Jo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
